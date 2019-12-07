1e5f40546c04c372b0e24bed76f352c257b660d8
Keith Kenny <keith.kny@gmail.com>
2019-12-07 11:20:16 +0000

    Big Juicy Update

--- TheSnow/Assets/SnowBall.cs
+++ TheSnow/Assets/SnowBall.cs
@@ -6,31 +6,38 @@ public class SnowBall : MonoBehaviour {
 
     [SerializeField]
     private float growthRate;
-
+    [SerializeField]
+    private float initalGrowthRate;
     private Rigidbody myRigidBoy;
 	// Use this for initialization
 	void Start ()
     {
         myRigidBoy = gameObject.GetComponent<Rigidbody>();
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
-            GetBigger();
+            AdjustSizeByGrowthRate();
         }
 	}
 
 
-    private void GetBigger()
+    private void AdjustSizeByGrowthRate()
     {
         var currentScale = transform.localScale;
         currentScale += new Vector3(currentScale.x * growthRate * Time.deltaTime, currentScale.y * growthRate * Time.deltaTime, currentScale.z * growthRate * Time.deltaTime);
         transform.localScale = currentScale;
     }
 
+
+    private void SetGrowthRate(float newGrowthRate)
+    {
+        growthRate = newGrowthRate;
+    }
 
 }
